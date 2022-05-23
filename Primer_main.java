2445d370dc92628a6c54d6656ac92be67330a28c
europa115 <estebanto92@gmail.com>
2022-05-23 18:02:28 +0200

    -añadida clase Clase_Counter.java -Uso de la Clase_Counter.java en Primer_main.java -cambio de nombre -añadido hola mundo.

--- Primer_main.java
+++ Primer_main.java
@@ -1,10 +1,6 @@
 public class Primer_main {
     public static void main(String[] args) {
 
-        System.out.println("Hola mundo");
-
-        System.out.println("Intento 2, vamos a la rama count");
-
         Counter.imprime_mil();
 
     }
